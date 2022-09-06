b84fd6f45ff171963b85758dbd7b5fdf028e4ad7
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-06 13:41:13 +0430

    Put added

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,20 +4,27 @@ from .serializers import PostSerializer
 from blog.models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-@api_view()
+@api_view(["GET", "POST"])
 def PostList(request):
-     posts = Post.objects.filter(status=True)
-     serializer = PostSerializer(posts,many=True)
-     return Response(serializer.data)
-
-
-
-@api_view()
-def PostDetail(request, id):
-     try:
-          # post = get_object_or_404(Post, pk=id) 
-          post = Post.objects.get(pk=id,status=True)
-          serializer = PostSerializer(post)
+     if request.method == "GET":
+          posts = Post.objects.filter(status=True)
+          serializer = PostSerializer(posts,many=True)
           return Response(serializer.data)
-     except Post.DoesNotExist:
-          return Response({'detail':'psot does not exist'},status=status.HTTP_404_NOT_FOUND)
+     elif request.method == "POST":
+          serializer = PostSerializer(data=request.data)
+          serializer.is_valid(raise_exception=True)
+          serializer.save()
+          return Response(serializer.data)
+
+@api_view(["GET", "PUT"])
+def PostDetail(request, id):
+     post = get_object_or_404(Post, pk=id,status=True)
+     if request.method == "GET":
+          serializer =PostSerializer(post)
+          return Response(serializer.data)
+     if request.method == "PUT":
+          serializer = PostSerializer(post,data=request.data)
+          serializer.is_valid(raise_exception=True)
+          serializer.save()
+          return Response(serializer.data)
+
