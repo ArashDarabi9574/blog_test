3a97262d406f883d4962951b9e9696df0f7b8453
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-09 13:15:59 +0430

    profile api added

--- core/blog/models.py
+++ core/blog/models.py
@@ -2,16 +2,15 @@ from unicodedata import category
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 # getting user model 
 # new branch commit
-User =  get_user_model()
+# User =  get_user_model()
 # Create your models here.
 class Post(models.Model):
      '''
      This's a class to define post for blog app
      '''
-     author = models.ForeignKey(User, on_delete=models.CASCADE)
+     author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
      image = models.ImageField(null=True, blank=True)
      title = models.CharField(max_length=255)
      content = models.TextField()
@@ -24,6 +23,8 @@ class Post(models.Model):
 
      def __st__(self):
           return self.title
+     def get_snippet(self):
+          return self.content[0:5]
 
 class Category(models.Model):
      name = models.CharField(max_length=255)
