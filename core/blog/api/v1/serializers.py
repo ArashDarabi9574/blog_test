90911f5ac606c8cb42f359d676e7f1543d77ca85
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-09 09:35:26 +0430

    category api added

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Category, Post
 class PostSerializer(serializers.ModelSerializer):
      class Meta:
           model = Post
           fields = ['id', 'author', 'title','category', 'content', 'status', 'created_date', 'published_data']
+
+class CategorySerializer(serializers.ModelSerializer):
+
+     class Meta:
+          model = Category
+          fields = ['id', 'name']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,23 +1,21 @@
 import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import PostSerializer
-from blog.models import Post
-from rest_framework import status
+from .serializers import PostSerializer,CategorySerializer
+from blog.models import Category, Post
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework import mixins
+from rest_framework import viewsets
 
-class PostList(ListCreateAPIView):
-     ''' getting a list of posts and creating posts '''
+class PostModelViewSet(viewsets.ModelViewSet):
      permission_classes = [IsAuthenticated]
      serializer_class = PostSerializer
      queryset = Post.objects.filter(status=True)
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-     ''' getting detail information of a post and updating/delete'''
+class CategoryModelViewSet(viewsets.ModelViewSet):
      permission_classes = [IsAuthenticated]
-     serializer_class = PostSerializer
-     queryset = Post.objects.filter(status=True)
+     serializer_class = CategorySerializer
+     queryset = Category.objects.all()
