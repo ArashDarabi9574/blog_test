ff680425cc4acf20b6e5114d1d47dbd11f7e456e
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-14 10:00:24 +0430

    create api registration addedd

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from . import views
 
+app_name = 'api-v1'
 urlpatterns = [
-
+     path('registration/', views.RegisterationApiView.as_view(), name='registration'),
 ]
