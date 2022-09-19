fb34423cd5f0f857871185f997c708130565cd2c
Arash Darabi-Nejad <arashdarabi9574@gmail.com>
2022-09-19 16:17:34 +0430

    faker

---
+++ core/blog/management/commands/insert-data.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from accounts.models import User,Profile
+from blog.models import Post,Category
+import random
+from datetime import datetime
+
+categort_list = [
+    'iot',
+    'other',
+    'backend',
+    'frontend'
+]
+class Command(BaseCommand):
+    help = 'inserting data into database'
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(email=self.fake.email(), password="test@1234")
+        profile = Profile.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.description = self.fake.paragraph(nb_sentences=5)
+        profile.save()
+
+        for name in categort_list:
+            Category.objects.get_or_create(name=name)
+
+        for _ in range(10):
+            Post.objects.create(
+                author = profile,
+                title = self.fake.paragraph(nb_sentences=1),
+                content = self.fake.paragraph(nb_sentences=10),
+                status = random.choice([True, False]),
+                category = Category.objects.get(name=random.choice(categort_list)),
+                published_data = datetime.now(),
+
+
+            )
